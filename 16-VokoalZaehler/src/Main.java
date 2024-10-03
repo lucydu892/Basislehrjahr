a70d74def5297d616ca7d90d32d0460689362efd
Lucy Duss <lucyduss@outlook.com>
2024-10-03 08:38:40 +0200

    aufgaben 16 version 1

--- 16-VokoalZaehler/src/Main.java
+++ 16-VokoalZaehler/src/Main.java
@@ -4,12 +4,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String test = "Dies ist ein Beispiel";
-        String[] ausgabe = test.split( "", 21);
-        for (String y : ausgabe) {
-            System.out.println(y);
-        }
-        //countletters(test, ausgabe);
+        System.out.println("Ich rechne dir die Anzahl der Buchstaben und wie viele Vokale vorkommen.");
+        System.out.println("Schreibe nun ein Wort oder ein Satz: ");
+        String sentance = scanner.nextLine();
+        int length = sentance.length();
+        countletters(sentance);
+        countVocals(length);
     }
-    //static int countletters (String test, String ausgabe String y) {}
+    static int countletters (String sentance) {
+        int length = 0;
+        for (int i = length; i < sentance.length(); i ++ ) {
+            if (sentance.charAt(i) != ' ') {
+                length++;
+                }
+        }
+        System.out.println("Dein Satz oder Wort besteht aus " + length + " Buchstaben");
+        return length;
+    }
+    static void countVocals (int length) {
+        System.out.println(length);
+    }
 }
