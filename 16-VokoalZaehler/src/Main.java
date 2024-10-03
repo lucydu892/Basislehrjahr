b8dfa62efafe9c6f7e074ae392977866aaabcff0
Lucy Duss <lucyduss@outlook.com>
2024-10-03 07:50:38 +0200

    aufgaben 16 test version

--- 16-VokoalZaehler/src/Main.java
+++ 16-VokoalZaehler/src/Main.java
@@ -1,7 +1,15 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String test = "Dies ist ein Beispiel";
+        String[] ausgabe = test.split( "", 21);
+        for (String y : ausgabe) {
+            System.out.println(y);
+        }
+        //countletters(test, ausgabe);
     }
+    //static int countletters (String test, String ausgabe String y) {}
 }
